b3132bf9ffabb862b374033fe8947407d5663fea
Umeitu <umeitupelpuo@gmail.com>
2022-03-23 11:29:44 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+function App() {
+const [title, setTitle]= useState("");
+const [body, setBody]= useState("");
+const [author, setAuthor] =useState("Ama")
+
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  const blog ={title, body, author};
+  console.log(blog);
+}
+
+return (
+<div className='create'>
+<h2>Add a new Blog</h2>
+
+<form onSubmit={handleSubmit}>
+<label>Blog Title</label>
+<input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)} />
+
+<label>Blog Body:</label>
+<textarea required value={body} onChange ={(e)=>setBody(e.target.value)}></textarea>
+
+<label>Blog Author</label>
+<select value={author} onChange ={(e)=>setAuthor(e.target.value)} >
+<option value="Ama">Ama</option>
+<option value="Francis">Francis</option>
+<option value="Stephen">Stephen</option>
+</select>
+
+<button>Add Blog</button>
+
+
+
+</form>    
+    
+</div>
+  )
+}
+
+export default App
